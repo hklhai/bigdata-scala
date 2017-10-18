3f94eaba4b746b820bb90c8d26acd84a58dbc92e
hklhai <hkhai@outlook.com>
2017-10-18 14:53:45 +0800

    action operation

--- src/main/scala/cn/edu/action/ActionOperation.scala
+++ src/main/scala/cn/edu/action/ActionOperation.scala
@@ -8,7 +8,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 object ActionOperation {
 
   def reduce(): Unit = {
-    val sc = new SparkContext(new SparkConf().setAppName("").setMaster("local"))
+    val sc = new SparkContext(new SparkConf().setAppName("reduce").setMaster("local"))
     val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
     val numbersRDD = sc.parallelize(numbers)
     val resRDD = numbersRDD.reduce(_ + _)
@@ -16,7 +16,62 @@ object ActionOperation {
 
   }
 
+  def collect(): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("collect").setMaster("local"))
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbersRDD = sc.parallelize(numbers)
+    val resRDD = numbersRDD.collect()
+    resRDD.foreach(e => println(e))
+  }
+
+  def count() = {
+    val sc = new SparkContext(new SparkConf().setAppName("count").setMaster("local"))
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbersRDD = sc.parallelize(numbers)
+    val sum = numbersRDD.count()
+    println(sum)
+  }
+
+  def take(): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("take").setMaster("local"))
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbersRDD = sc.parallelize(numbers)
+    val resRDD = numbersRDD.take(6)
+    resRDD.foreach(e => println(e))
+  }
+
+  def saveAsTextFile(): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("saveAsTextFile").setMaster("local"))
+    val numbers = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val numbersRDD = sc.parallelize(numbers)
+    numbersRDD.saveAsTextFile("hdfs://spark01:9000/scala_numbers")
+  }
+
+  def countByKey(): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("saveAsTextFile").setMaster("local"))
+    val numbers = Array(
+      Tuple2("class1", "leo"),
+      Tuple2("class2", "jack"),
+      Tuple2("class1", "marry"),
+      Tuple2("class2", "tom"),
+      Tuple2("class2", "david"));
+    val numRDD = sc.parallelize(numbers)
+    val res = numRDD.countByKey()
+    println(res)
+
+  }
+
   def main(args: Array[String]): Unit = {
-    reduce()
+    //    reduce()
+
+    //    collect()
+
+    //    count()
+
+    //    take()
+
+    saveAsTextFile()
+
+    //    countByKey()
   }
 }
